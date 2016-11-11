58a63880bd3dada7821b25ee7a9f8d429968d417
Matt Cameron <matt@mtmc.ca>
2016-11-11 01:07:41 -0700

    sessions controller and views added

--- app/controllers/driveable_controller.rb
+++ app/controllers/driveable_controller.rb
@@ -1,8 +1,62 @@
 class DriveableController < ApplicationController
 
   before_filter :authorize
-  
+
   def dashboard
+    require 'aws-sdk-rails'
+
+    @ec2 = Aws::EC2::Resource.new(region: 'us-east-1')
   end
+
+  def instanceprofile
+    require 'aws-sdk-rails'
+
+    @ec2 = Aws::EC2::Resource.new(region: 'us-east-1')
+  end
+
+  def instancelisting
+  end
+
+  def stop
+   require 'aws-sdk-rails'
+
+   ec2 = Aws::EC2::Resource.new(region: 'us-east-1')
+   target = params[:instance]
+   i = ec2.instance(target)
+
+   if i.exists?
+     case i.state.code
+     when 80  # stopped
+       puts "#{i.id} is stopped, so you cannot stop it"
+     when 48  # terminated
+       puts "#{i.id} is terminated, so you cannot start it"
+     else
+       i.stop
+       redirect_to "/"
+     end
+     end
+  end
+
+  def start
+    require 'aws-sdk-rails'
+
+    ec2 = Aws::EC2::Resource.new(region: 'us-east-1')
+    target = params[:instance]
+    i = ec2.instance(target)
+
+    if i.exists?
+      case i.state.code
+      when 0  # pending
+        puts "#{i.id} is pending, so it will be running in a bit"
+      when 16  # started
+        puts "#{i.id} is already started"
+      when 48  # terminated
+        puts "#{i.id} is terminated, so you cannot start it"
+      else
+        i.start
+        redirect_to "/"
+      end
+      end
+   end
 
 end
