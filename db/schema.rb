1c4184effa63dc338c720597de8207af240354a4
Matt Cameron <matt@mtmc.ca>
2016-11-11 06:55:50 +0000

    user model updated

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20161111064938) do
+
+  create_table "users", force: :cascade do |t|
+    t.string   "fname",           limit: 255
+    t.string   "lname",           limit: 255
+    t.string   "email",           limit: 255
+    t.string   "password_digest", limit: 255
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+  end
 
 end
